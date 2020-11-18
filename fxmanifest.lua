b256aa136e016c4bac11f11b55053764b9eb2e85
nevozy <nevo020806@gmail.com>
2020-11-18 10:14:46 +0200

    fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,3 +1,11 @@
+fx_version 'bodacious'
+
+games {"gta5"}
+
+author "Nevo#2020"
+description "Advanced Active Officers System"
+version "1.0.0"
+
 ui_page "html/index.html"
 
 shared_script "config.lua"
